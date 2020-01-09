821cf757d478c83404547e816aa9a54bedbfe27e
Marcus Reinhardt <webstone@gmail.com>
2020-01-09 22:54:53 +0100

    styling

--- src/main.js
+++ src/main.js
@@ -1,37 +1,20 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import DefaultLayout from '~/layouts/Default.vue'
 import VTooltip from 'v-tooltip'
 
-import {
-  FontAwesomeIcon
-} from '@fortawesome/vue-fontawesome';
-import {
-  config,
-  library
-} from '@fortawesome/fontawesome-svg-core';
-import {
-  fas
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  fab
-} from '@fortawesome/free-brands-svg-icons'
+import '~/assets/scss/main.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
-
-import '~/assets/scss/main.scss';
 
 config.autoAddCss = false;
 library.add(fas);
 library.add(fab);
 
-export default function (Vue, {
-  router,
-  head,
-  isClient
-}) {
-  // Set default layout as a global component
+export default function (Vue, {router, head, isClient}) {
   Vue.component('Layout', DefaultLayout)
 
   Vue.component('font-awesome', FontAwesomeIcon)
@@ -40,5 +23,4 @@ export default function (Vue, {
     defaultPlacement: 'top-end',
     defaultClass: 'bg-black text-xs px-2 leading-normal py-1 rounded absolute text-gray-400 max-w-xs mb-1'
   })
-
 }
